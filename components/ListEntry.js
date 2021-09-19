2037ca041720d840def1cd485ca726cb60b08831
kaiming <57194998+kaiming@users.noreply.github.com>
2021-09-18 20:12:27 -0400

    created listEntry component

---
+++ components/ListEntry.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ListEntry = () => {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.titleView}>
+                <View style={{flex: 1}}>
+                    <Text style={styles.name}>John Green</Text>
+                </View>
+                <View style={{flex: 1}}>
+                    <Text style={styles.date}>Mar 2021</Text>
+                </View>
+            </View>
+            <View style={styles.noteView}>
+                <Text style={styles.note}>alsdfkjghasgljsdgljshdkgasdg dsgh jhdskgja dfasda fsadf dfas dfasd fsdaf dasf adsf sadf dsaf </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#EDEDED',
+        overflow: 'hidden',
+        borderRadius: 10
+    },
+
+    titleView: {
+        flexDirection: 'row'
+    },
+
+    noteView: {
+        paddingHorizontal: 15,
+        paddingBottom: 20,
+        alignSelf: 'flex-start'
+    },
+
+    name: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        paddingHorizontal: 15,
+        paddingVertical: 20
+    },
+
+    date: {
+        fontSize: 16,
+        color: '#FE4B03',
+        textAlign: 'right',
+        paddingHorizontal: 15,
+        paddingVertical: 20
+    },
+
+    note: {
+
+    },
+})
+
+export default ListEntry;
